81146c29431d44423b8d5a4d2793da5422fb3679
hbwqing1017 <510005458@qq.com>
2018-01-16 10:44:15 +0800

    提交
    提交

---
+++ Demo_1/Assets/AudioRes/AudioTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTest : MonoBehaviour {
+    public AudioSource staticAudioSource;
+    public AudioClip staticAudoClip;
+    AudioSource audioSource;
+    GameObject audioGo;
+    public void BtnClick()
+    {
+        if (staticAudioSource == null)
+        {
+            if (audioSource == null)
+            {
+                GameObject go = Resources.Load("Prefabs_Sound/UI_button") as GameObject;
+                audioGo = Instantiate(go) as GameObject;
+                audioSource = audioGo.GetComponent<AudioSource>();
+            }
+            audioSource.Play();
+        }
+        else
+        {
+            staticAudioSource.clip = staticAudoClip;
+            staticAudioSource.Play();
+        }
+
+        Debug.Log("BtnClick");
+    }
+}
